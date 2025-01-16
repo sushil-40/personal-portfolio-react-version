76a7135d0ebe9d9edb8eabe9e3ab3609b5101b60
Sushil <grayson.9999@gmail.com>
2025-01-16 21:57:06 +1000

    adding Offcanvas feature in project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import flickstoreImage from "../assets/flickstore.png";
 import prankcalculatorImage from "../assets/prankcalculator.png";
 import timewiseImage from "../assets/timewise.png";
@@ -34,8 +34,11 @@ export const Projects = () => {
       img: { path: contactlistImage, alt: "project_4" },
     },
   ];
-  console.log(projectList);
 
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const toggleOffcanvas = () => {
+    setShowOffcanvas(!showOffcanvas);
+  };
   return (
     <section id="projects">
       <Parallax backgroundImage={projectParallaxImage} title={"My Projects"} />
@@ -45,14 +48,14 @@ export const Projects = () => {
         <div className="accordion-item">
           <h2 className="accordion-header">
             <button
-              className="accordion-button"
+              className="accordion-button "
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseOne"
               aria-expanded="true"
               aria-controls="collapseOne"
             >
-              Completed Projet
+              <span className="title completed-project">Completed Project</span>
             </button>
           </h2>
           <div
@@ -72,46 +75,70 @@ export const Projects = () => {
                         // width={50}
                       />
                       <div className="project-content">
+                        <div className="links">
+                          <a href="">
+                            <i className="fa-brands fa-github"></i>{" "}
+                          </a>
+                          <a href="">
+                            <i className="fa-brands fa-chrome"></i>
+                          </a>
+                        </div>
+
                         <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <a href="#" className="btn-tertiary">
+                        <p>{project.description.slice(0, 100) + " ...."}</p>
+                        <button
+                          className="btn project-btn btn-tertiary"
+                          onClick={toggleOffcanvas}
+                          type="button"
+                          data-bs-toggle="offcanvas"
+                          data-bs-target="#offcanvasWithBothOptions"
+                          aria-controls="offcanvasWithBothOptions"
+                        >
                           View Project
-                        </a>
+                        </button>
+                        {/* <div className="project-btn">
+                          <a href="#" className="btn-tertiary">
+                            View Project
+                            
+                          </a>
+                        </div> */}
                       </div>
                     </div>
                   );
                 })}
+                {/* test  */}
 
-                {/* <div className="project">
-          <img src="project2.jpg" alt="Project 2" />
-          <div className="project-content">
-            <h3>Project Two</h3>
-            <p>An e-commerce platform for buying and selling vintage items.</p>
-            <a href="#" className="btn-tertiary">
-              View Project
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <img src="project1.jpg" alt="Project 1" />
-          <div className="project-content">
-            <h3>Project One</h3>
-            <p>A modern and sleek movie search application built with React.</p>
-            <a href="#" className="btn-tertiary">
-              View Project
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <img src="project2.jpg" alt="Project 2" />
-          <div className="project-content">
-            <h3>Project Two</h3>
-            <p>An e-commerce platform for buying and selling vintage items.</p>
-            <a href="#" className="btn-tertiary">
-              View Project
-            </a>
-          </div>
-        </div> */}
+                <div
+                  className={`offcanvas offcanvas-start w-50  text-bg-dark ${
+                    showOffcanvas ? "show" : ""
+                  }`}
+                  data-bs-scroll="true"
+                  tabIndex="-1"
+                  id="offcanvasWithBothOptions"
+                  aria-labelledby="offcanvasWithBothOptionsLabel"
+                >
+                  <div className="offcanvas-header">
+                    <h5
+                      className="offcanvas-title"
+                      id="offcanvasWithBothOptionsLabel"
+                    >
+                      Backdrop with scrolling
+                    </h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      onClick={toggleOffcanvas}
+                      data-bs-dismiss="offcanvas"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="offcanvas-body">
+                    <p>
+                      Try scrolling the rest of the page to see this option in
+                      action.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -126,7 +153,9 @@ export const Projects = () => {
               aria-expanded="false"
               aria-controls="collapseTwo"
             >
-              Running Project
+              <span className="title running-project" style={{ width: "80%" }}>
+                Running Projects
+              </span>
             </button>
           </h2>
           <div
