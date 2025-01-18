4062b92e7d6fbed485e982df7f620f7cff422986
Sushil <grayson.9999@gmail.com>
2025-01-18 22:02:39 +1000

    adding dice face

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import AnimatedIcons from "./AnimatedIcon";
 import { Dice } from "./Dice";
 
 export const Hero = () => {
+  const [showButtonRoll, setShowButtonRoll] = useState("hidden-dice");
+
+  const handleOnClickReveal = () => {
+    setShowButtonRoll((prevClass) =>
+      prevClass === "hidden-dice" ? "show-hidden-dice" : "hidden-dice"
+    );
+  };
   return (
     <section id="hero" className="d-flex">
       {/* 
@@ -36,14 +43,24 @@ export const Hero = () => {
             Download Resume
           </a>
         </div>
-        <div className="right-content col-md-3">
-          <div className="cube">
-            <div className="face front"></div>
+        <div className="right-content col-md-4 flex-column">
+          <div className="cube m-5">
+            <div className="face front"> </div>
             <div className="face back"></div>
             <div className="face left"></div>
             <div className="face right"></div>
             <div className="face top"></div>
             <div className="face bottom"></div>
+          </div>
+          <div className="btn-magic  p-5">
+            <p className={showButtonRoll}>
+              <button>Roll Dice</button>
+            </p>
+            <p>
+              <button onClick={handleOnClickReveal} className="btn btn-info">
+                Toggle To Reveal
+              </button>
+            </p>
           </div>
         </div>
         {/* <div className="parallax"></div> */}
