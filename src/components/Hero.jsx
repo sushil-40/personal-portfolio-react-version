42e5d6728d5da32b3e40c9f00c0e220ec2c08693
Sushil <grayson.9999@gmail.com>
2025-01-25 20:55:10 +1000

    fixing media queries

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -87,114 +87,118 @@ export const Hero = () => {
   return (
     <section id="hero" className="d-flex">
       <div className="container hero-container d-flex">
-        <div className="left-content col-md-6 ">
-          <h1 className=" name fadeInRight font-effect-shadow-multiple">
-            Sushil Dangoriya
-          </h1>
-          <hr className="m-1 hr-line" />
-          <h2 className=" job-title fadeInLeft font-effect-shadow-multiple">
-            Web Developer & Designer
-          </h2>
-          <p className="lead animated fadeInRight">
-            Transforming ideas into interactive digital experiences.
-          </p>
-          <a
-            href="#projects"
-            className="btn btn-primary btn-lg animated fadeInUp "
-          >
-            Explore My Work
-          </a>
-          <a
-            href="/file-assets/sushil-resume.pdf"
-            download="sushil-resume.pdf"
-            className="btn btn-primary btn-lg animated fadeInUp"
-          >
-            Download Resume
-          </a>
-        </div>
-        <div className="right-content col-md-4 flex-column">
-          <div className={`cube m-5 ${animationClass}`}>
-            <div
-              className={`face front ${
-                isBlurred && resultFace !== 5 ? "blur" : ""
-              }`}
-              style={{
-                backgroundImage:
-                  showButtonRoll === "show-hidden-dice"
-                    ? `url(${diceFace1})`
-                    : "",
-                opacity: showButtonRoll === "show-hidden-dice" ? "0.8" : "1",
-                // background: `${cubeStyle.color}`,
-              }}
-            ></div>
-            <div
-              className={`face back ${
-                isBlurred && resultFace !== 5 ? "blur" : ""
-              }`}
-              style={{
-                backgroundImage:
-                  showButtonRoll === "show-hidden-dice"
-                    ? `url(${diceFace2})`
-                    : "",
-              }}
-            ></div>
-            <div
-              className={`face left ${
-                isBlurred && resultFace !== 5 ? "blur" : ""
-              }`}
-              style={{
-                backgroundImage:
-                  showButtonRoll === "show-hidden-dice"
-                    ? `url(${diceFace3})`
-                    : "",
-              }}
-            ></div>
-            <div
-              className={`face right ${
-                isBlurred && resultFace !== 5 ? "blur" : ""
-              }`}
-              style={{
-                backgroundImage:
-                  showButtonRoll === "show-hidden-dice"
-                    ? `url(${diceFace4})`
-                    : "",
-              }}
-            ></div>
-            <div
-              className="face top"
-              style={{
-                backgroundImage:
-                  showButtonRoll === "show-hidden-dice"
-                    ? `url(${topFaceImage})`
-                    : "",
-              }}
-            ></div>
-            <div
-              className={`face bottom ${
-                isBlurred && resultFace !== 5 ? "blur" : ""
-              }`}
-              style={{
-                backgroundImage:
-                  showButtonRoll === "show-hidden-dice"
-                    ? `url(${diceFace6})`
-                    : "",
-              }}
-            ></div>
+        <div className="left-container col-md-6 ">
+          <div className="left-content ">
+            <h1 className=" name fadeInRight font-effect-shadow-multiple">
+              Sushil Dangoriya
+            </h1>
+            <hr className="m-1 hr-line" />
+            <h2 className=" job-title fadeInLeft font-effect-shadow-multiple">
+              Web Developer & Designer
+            </h2>
+            <p className="lead animated fadeInRight">
+              Transforming ideas into interactive digital experiences.
+            </p>
+            <a
+              href="#projects"
+              className="btn btn-primary btn-lg animated fadeInUp "
+            >
+              Explore My Work
+            </a>
+            <a
+              href="/file-assets/sushil-resume.pdf"
+              download="sushil-resume.pdf"
+              className="btn btn-primary btn-lg animated fadeInUp"
+            >
+              Download Resume
+            </a>
           </div>
-          <div className="btn-magic  p-5">
-            <p className={showButtonRoll}>
-              <button
-                onClick={handleOnRollDiceClicked}
-                className="btn btn-dark roll-dice"
-              >
-                Roll Dice
-              </button>
-            </p>
-            <p>
-              <button onClick={handleOnClickReveal} className="btn btn-info">
-                Toggle To Reveal
-              </button>
-            </p>
+        </div>
+        <div className="right-container col-md-4 flex-column">
+          <div className="right-content">
+            <div className={`cube m-5 ${animationClass}`}>
+              <div
+                className={`face front ${
+                  isBlurred && resultFace !== 5 ? "blur" : ""
+                }`}
+                style={{
+                  backgroundImage:
+                    showButtonRoll === "show-hidden-dice"
+                      ? `url(${diceFace1})`
+                      : "",
+                  opacity: showButtonRoll === "show-hidden-dice" ? "0.8" : "1",
+                  // background: `${cubeStyle.color}`,
+                }}
+              ></div>
+              <div
+                className={`face back ${
+                  isBlurred && resultFace !== 5 ? "blur" : ""
+                }`}
+                style={{
+                  backgroundImage:
+                    showButtonRoll === "show-hidden-dice"
+                      ? `url(${diceFace2})`
+                      : "",
+                }}
+              ></div>
+              <div
+                className={`face left ${
+                  isBlurred && resultFace !== 5 ? "blur" : ""
+                }`}
+                style={{
+                  backgroundImage:
+                    showButtonRoll === "show-hidden-dice"
+                      ? `url(${diceFace3})`
+                      : "",
+                }}
+              ></div>
+              <div
+                className={`face right ${
+                  isBlurred && resultFace !== 5 ? "blur" : ""
+                }`}
+                style={{
+                  backgroundImage:
+                    showButtonRoll === "show-hidden-dice"
+                      ? `url(${diceFace4})`
+                      : "",
+                }}
+              ></div>
+              <div
+                className="face top"
+                style={{
+                  backgroundImage:
+                    showButtonRoll === "show-hidden-dice"
+                      ? `url(${topFaceImage})`
+                      : "",
+                }}
+              ></div>
+              <div
+                className={`face bottom ${
+                  isBlurred && resultFace !== 5 ? "blur" : ""
+                }`}
+                style={{
+                  backgroundImage:
+                    showButtonRoll === "show-hidden-dice"
+                      ? `url(${diceFace6})`
+                      : "",
+                }}
+              ></div>
+            </div>
+            <div className="btn-magic  p-5">
+              <p className={showButtonRoll}>
+                <button
+                  onClick={handleOnRollDiceClicked}
+                  className="btn btn-dark roll-dice"
+                >
+                  Roll Dice
+                </button>
+              </p>
+              <p>
+                <button onClick={handleOnClickReveal} className="btn btn-info">
+                  Toggle To Reveal
+                </button>
+              </p>
+            </div>
           </div>
         </div>
       </div>
